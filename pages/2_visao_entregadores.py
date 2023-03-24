cdeb5b625d908bdb1505951c25110bd82fee8705
Joao D Martire <70855429+jhmartire@users.noreply.github.com>
2023-03-24 19:44:33 +0000

    Update 2_visao_entregadores.py

--- pages/2_visao_entregadores.py
+++ pages/2_visao_entregadores.py
@@ -102,7 +102,7 @@ def clean_code( df1 ):
 # IMPORT DATASET
 # df = pd.read_csv('../train.csv')
 #df = pd.read_csv('ftc/train.csv')
-df = pd.read_csv('/Users/jhmartire/repos/ftc/train.csv')
+df = pd.read_csv('dataset/train.csv')
 
 
 #CLEANING DATASET
