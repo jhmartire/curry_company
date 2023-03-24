bd31879b36637cf43ef9518143570847e4279f3a
Joao D Martire <70855429+jhmartire@users.noreply.github.com>
2023-03-24 19:45:06 +0000

    Update 3_visao_restaurante.py

--- pages/3_visao_restaurante.py
+++ pages/3_visao_restaurante.py
@@ -157,7 +157,7 @@ def clean_code( df1 ):
 
 # df = pd.read_csv('../train.csv')
 #df = pd.read_csv('ftc/train.csv')
-df = pd.read_csv('/Users/jhmartire/repos/ftc/train.csv')
+df = pd.read_csv('dataset/train.csv')
 
 
 #CLEANING DATAFRAME
